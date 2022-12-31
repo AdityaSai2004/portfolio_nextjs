0567b739e6fd5fa2b9a9f54a1182e98bc137ae0d
AdityaSai2004 <2004sai@gmail.com>
2022-12-31 23:54:40 +0530

    Added project template

--- components/projects.jsx
+++ components/projects.jsx
@@ -5,7 +5,7 @@ import propertyImg from "../public/assets/projects/property.jpg";
 import cryptoImg from "../public/assets/projects/crypto.jpg";
 import netflixImg from "../public/assets/projects/netflix.jpg";
 import twitchImg from "../public/assets/projects/twitch.jpg";
-import ProjectItem from "./ProjectItem";
+import ProjectItem from "./ProjectItem.jsx";
 
 const Projects = () => {
   return (
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from "../components/navbar";
 import Main from "../components/main";
 import About from "../components/about";
 import Skills from "../components/skills";
+import Projects from "../components/projects";
 export default function Home() {
   return (
     <div>
@@ -16,6 +17,7 @@ export default function Home() {
       <Main />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
